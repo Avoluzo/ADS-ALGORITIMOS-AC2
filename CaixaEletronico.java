f2512411c556fcbd7075b6485cde74f220bb0f7b
Avoluzo <guguavoluzo@gmail.com>
2022-11-14 19:34:39 -0300

    adicionando implementacao  Caixa eletronico

--- CaixaEletronico.java
+++ CaixaEletronico.java
@@ -37,9 +37,10 @@ public class CaixaEletronico {
         }
 
         float balance = 1000.0f;
+        StringBuilder extractBuilder = new StringBuilder("extrato: \n");
         while (true) {
 
-            System.out.println("MENU:\n - 1: saldo \n - 2: deposito \n - 3: saque \n - 0: sair ");
+            System.out.println("\nMENU:\n - 1: saldo \n - 2: deposito \n - 3: saque \n - 4: transferencia \n - 5: extrato \n - 0: sair ");
             int menu = scanner.nextInt();
 
             switch (menu) {
@@ -53,13 +54,50 @@ public class CaixaEletronico {
                     float deposit = scanner.nextFloat();
                     balance = balance + deposit;
                     System.out.println("o valor de seu saldo atual, em reais é de :" + balance);
-
+                    extractBuilder.append("deposito realizado no valor: ");
+                    extractBuilder.append(deposit);
+                    extractBuilder.append("\nsaldo: ");
+                    extractBuilder.append(balance);
+                    extractBuilder.append("\n\n");
                     break;
                 case 3:
                     System.out.print("Digite o valor desejado: ");
                     float withdraw = scanner.nextFloat();
                     balance = balance - withdraw;
                     System.out.println("o valor de seu saldo atual, em reais é de :" + balance);
+                    extractBuilder.append("saque realizado no valor: ");
+                    extractBuilder.append(withdraw);
+                    extractBuilder.append("\nsaldo: ");
+                    extractBuilder.append(balance);
+                    extractBuilder.append("\n\n");
+
+                    break;
+                case 4:
+                    System.out.print("Digite o valor a ser transferido: ");
+                    float transfer = scanner.nextFloat();
+                    System.out.print("Digite a conta: ");
+                    int account = scanner.nextInt();
+                    System.out.print("Digite a agencia: ");
+                    int agency = scanner.nextInt();
+                    System.out.print("Digite o banco: ");
+                    int bank = scanner.nextInt();
+                    balance = balance - transfer;
+                    extractBuilder.append("transferencia realizado no valor: ");
+                    extractBuilder.append(transfer);
+                    extractBuilder.append(" para a conta:  ");
+                    extractBuilder.append(account);
+                    extractBuilder.append(" agencia: ");
+                    extractBuilder.append(agency);
+                    extractBuilder.append(" banco: ");
+                    extractBuilder.append(bank);
+                    extractBuilder.append("\nsaldo: ");
+                    extractBuilder.append(balance);
+                    extractBuilder.append("\n\n");
+
+                    break;
+
+                case 5:
+                    System.out.print(extractBuilder);
 
                     break;
                 case 0:
